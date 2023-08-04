03957dfe60714645bc1b5bc819f09c5d6079e57b
qyx101 <qyx_1001@163.com>
2023-08-05 06:32:11 +0800

    8-4

---
+++ 2023-8/8-4/t1.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+//
+////int main()
+////{
+////	printf("hello bit\n");
+////	return 0;
+////}
+//
+// 
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("     **     \n");
+//    printf("     **     \n");
+//    printf("************\n");
+//    printf("************\n");
+//    printf("    *  *    \n");
+//    printf("    *  *    \n");
+//}
+//#include <stdio.h>
+//
+//int main() {
+//    int a = 0;
+//    scanf("%d", &a);
+//    if (a >= 140)
+//    {
+//        printf("Genius");
+//    }
+//
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf_s("%d %d", &a, &b);
+//	printf("%d", a + b);
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//    int a = 0;
+//    scanf("%d", &a);
+//    if (a % 5 == 0)
+//    {
+//        printf("YES");
+//    }
+//    else {
+//        printf("NO");
+//    }
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//    int a, b;
+//    while (scanf("%d", &a) != EOF) { // 注意 while 处理多个 case
+//        if (a % 2 == 0) {
+//            printf("Even\n");
+//        }
+//        else {
+//            printf("Odd\n");
+//        }
+//    }
+//    return 0;
+//}
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//    char arr1[] = { 'b', 'i', 't' };
+//    char arr2[] = "bit";
+//    printf("%d\n", strlen(arr2));
+//    return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//    int n = 0;
+//    scanf("%d", &n);
+//    long sum = 0;
+//    while (0 < n) {
+//        sum += n;
+//        n--;
+//    }
+//    printf("%ld\n", sum);
+//}
+
+int main()
+{
+	printf("\a");
+}
