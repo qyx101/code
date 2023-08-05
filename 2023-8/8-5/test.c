be606cb1800f447e51db1a88e52b7599287d535c
qyx101 <qyx_1001@163.com>
2023-08-05 22:07:15 +0800

    8-5

---
+++ 2023-8/8-5/test.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//int main() {
+//    char ch;
+//    while (scanf("%c", &ch) != -1)
+//    {
+//        if (ch >= 65 && ch <= 90)
+//        {
+//            ch += 32;
+//        }
+//        else if (ch >= 97 && ch <= 122)
+//        {
+//            ch -= 32;
+//        }
+//        printf("%c", ch);
+//    }
+//}
+
+//int main() {
+//    char ch;
+//    while (scanf("%c", &ch) != EOF) { // ע�� while ������� case
+//        if (ch >= 65 && ch <= 122) {
+//            printf("%c is an alphabet.\n", ch);
+//        }
+//        else {
+//            printf("%c is not an alphabet.\n", ch);
+//        }
+//        getchar();
+//    }
+//    return 0;
+//}
