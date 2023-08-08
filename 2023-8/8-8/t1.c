2c1813c49c204ec76207aeb689d7eee1081a0821
qyx101 <qyx_1001@163.com>
2023-08-08 21:31:03 +0800

    8-8

---
+++ 2023-8/8-8/t1.c
@@ -0,0 +1,76 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+
+//// ��ӡ���� 100-200
+//int is_prime(int n)
+//{
+//	for (int i = 2; i < n; i++)
+//	{
+//		if (n % i == 0)
+//		{
+//			return 0; 
+//		}
+//	}
+//	return 1;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i < 200; i++)
+//	{
+//		if (is_prime(i))
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//}
+//void t()
+//{
+//	/*{
+//		int  a = 1;
+//	}*/
+//	printf("1");
+//}
+//int main()
+//{
+//	t();
+//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//void print(int n)
+//{
+//	int i = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; i >= j; j++)
+//		{
+//			printf("%d * %d = %d ", j, i, i * j);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print(n);
+//}
+
+//void swap(int* pa, int* pb)
+//{
+//	int tmp = *pa;
+//	*pa = *pb;	
+//	*pb = tmp;
+//}
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	swap(&a, &b);
+//	printf("a = %d b = %d", a, b);
+//}
+
+
