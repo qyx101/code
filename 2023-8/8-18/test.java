edd167107a95dd8a630d5d0b04af601ed9c3f04d
qyx101 <qyx_1001@163.com>
2023-08-18 20:51:59 +0800

    8-18

---
+++ 2023-8/8-18/test.java
@@ -0,0 +1,10 @@
+public class TestDemo {
+    // m
+    public static void main(String[] args) {
+        // sout
+        //System.out.println("hello");
+        // short 2 < int 4 < long 8
+        // int a = 10;
+        System.out.println('a');
+    }
+}
